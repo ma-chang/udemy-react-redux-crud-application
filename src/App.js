3df6fab9ec0c45604e02d1234589874d4ab8c079
ma-chang <akihisa.bamba.10@gmail.com>
2021-10-23 15:48:21 +0900

    lear prop-type

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 // transpileに import Reactが必須。jsxがなければ不要
 // import React, { Component } from "react";
 import React from "react";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input
-//           type="text"
-//           onClick={() => {
-//             console.log("I'm clicked");
-//           }}
-//           onChange={() => {
-//             console.log("I'm changed");
-//           }}
-//         />
-//       </React.Fragment>
-//     );
-//   }
-// }
+import PropTypes from "prop-types";
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName", age: 99 },
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!</div>;
+const User = (props) => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old.
+    </div>
+  );
+};
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
 };
 
 export default App;
